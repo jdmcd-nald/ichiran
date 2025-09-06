1407f34582a0c7225d2998e2e830deba619a0c79
Jared McDonald <120195133+jdmcd-nald@users.noreply.github.com>
2025-09-06 15:51:53 -0700

    Update to cli.lisp
    includes parenthesizing additions

--- cli.lisp
+++ cli.lisp
@@ -45,12 +45,131 @@
   (loop for (word . gloss) in info
         do (format t "~%~%* ~a  ~a" word gloss)))
 
+(defun first-non-char (key)
+  (defparameter num 0)
+  (defparameter multi-check 0)
+  (loop for i from 0 to (- (length key) 1) do
+     
+
+     
+     (if (and (> num 0) (= multi-check 3))
+        (setq num (- num multi-check))
+     )
+     (if (and (> num 0) (= multi-check 3))
+         (setq multi-check 0)
+     )
+     (if (and (string= (aref key i) "\.") (= num 0) (= multi-check 2))
+        (setq multi-check (+ multi-check 1))
+     )
+     (if (and (string= (aref key i) ">") (= num 0) (= multi-check 1))
+        (setq multi-check (+ multi-check 1))
+     )
+     (if (and (string= (aref key i) "<") (= num 0) (= multi-check 0))
+        (setq multi-check (+ multi-check 1))
+     )
+         
+     (if (or (string= (aref key i) " ") (string= (aref key i) #\Newline))
+       (if (= num 0)
+         (setq num i)
+       )
+     )
+  )
+
+  (* num 1)
+)
+
+(defun first-non-char-start (key)
+       
+  (defparameter num 0)
+          
+  (defparameter multi-check 0)
+           
+  (loop for i from 0 to (- (length key) 1) do
+     
+     
+     
+     (if (and (> num 0) (= multi-check 3))
+         (- num multi-check)
+     )
+     
+     (if (and (string= (aref key i) ".") (= num 0) (= multi-check 3))
+         (setq multi-check(+ multi-check 2))
+     )
+    
+     (if (and (string= (aref key i) ">") (= num 0) (= multi-check 1))
+                 (setq multi-check(+ multi-check 2))
+     )
+     
+     (if (and (string= (aref key i) "<") (= num 0) (= multi-check 0))
+                  (setq multi-check(+ multi-check 1))
+     )
+     
+     
+     (if (and (> i multi-check) (or (string= (aref key i) " ") (string= (aref key i) #\Newline)))
+       (if (= num 0)
+         (setq num i)
+       )
+     )
+  )
+  (* multi-check 1)
+)
+
+(defun print-input-w-parentheses-2 (input charp charl current-print shortinput)
+  
+  (loop for i from 0 to (- current-print 1) do
+     (princ (aref input i))
+  )
+  (loop for i from 0 to (- (length shortinput) 1) do
+     (if (= i charp) ( princ "\(" ))
+     (princ (aref shortinput i))
+     (if (= i (- (+ charp charl) 1)) ( princ "\)" ))
+     (if (= i (- (+ charp charl) 1)) ( setq  current-print (+ current-print i) ))
+  )
+  (* current-print 1)
+)
+
+(defun print-input-w-parentheses (input charp charl)
+  
+  (loop for i from 0 to (- (length input) 1) do
+     (if (= i charp) ( princ "\(" ))
+     (princ (aref input i))
+     (if (= i (- (+ charp charl) 1)) ( princ "\)" ))
+  )
+)
+
+(defun print-romanize-info-w-input (info input r)
+  (defparameter charl 0)
+  (defparameter charp 0)
+  (defparameter charstart 0)
+  
+  (defparameter shortinput "h")
+  (defparameter current-print 0)
+  
+  (loop for (word . gloss) in info
+        do (format t "~%~%~%*")
+            
+           (setq charl (first-non-char gloss))
+           
+           (setq charstart  (first-non-char-start gloss))
+           
+           (setq charp (search (subseq gloss charstart (+ charstart charl)) input))     
+           ;;(print-input-w-parentheses input charp charl)                           
+           (setq shortinput (subseq input current-print (length input)))
+           
+           (setq charp (search (subseq gloss charstart (+ charstart charl)) shortinput))
+           (setq current-print (print-input-w-parentheses-2 input charp charl current-print shortinput))
+           (format t "*~% ~a  ~a" word gloss)
+           
+           
+  )
+)
+
 (defun main ()
   (load-connection-from-env)
   (multiple-value-bind (options free-args)
       (handler-case
-        (handler-bind ((opts:unknown-option #'unknown-option))
-          (opts:get-opts))
+          (handler-bind ((opts:unknown-option #'unknown-option))
+            (opts:get-opts))
 
         (opts:missing-arg (condition)
           (print-error "fatal: option ~s needs an argument!~%"
@@ -79,7 +198,7 @@
        (let ((input (join " " free-args)))
          (multiple-value-bind (r info) (romanize input :with-info t)
            (princ r)
-           (print-romanize-info info))))
+           (print-romanize-info-w-input info input r))))
       ((getf options :full)
        (let* ((input (join " " free-args))
               (limit-value (getf options :limit))
@@ -90,6 +209,7 @@
       ))
   (terpri)
   (finish-output))
+
 
 
 (defun setup-debugger ()
